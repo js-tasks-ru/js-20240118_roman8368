32b410a34b5e667898fe44e11c43dbc3bc4f4222
oqo0 <oq.roman@gmail.com>
2024-01-29 19:23:09 +0300

    1 task

--- 02-javascript-data-types/1-sort-strings/index.js
+++ 02-javascript-data-types/1-sort-strings/index.js
@@ -4,6 +4,22 @@
  * @param {string} [param="asc"] param - the sorting type "asc" or "desc"
  * @returns {string[]}
  */
-export function sortStrings(arr, param = 'asc') {
+function sortStrings(arr, param = 'asc') {
+    switch (param) {
+        case 'asc':
+            return sortStringsAscending(arr);
+        case 'desc':
+            return sortStringsDescending(arr);
+        default:
+            return arr;
+    }
+}
 
+function sortStringsAscending(arr) {
+    return arr.sort((a, b) =>
+    a.toLowerCase().localeCompare(b.toLowerCase(), ['ru', 'en']));
+}
+
+function sortStringsDescending(arr) {
+    return sortStringsAscending(arr).reverse();
 }
