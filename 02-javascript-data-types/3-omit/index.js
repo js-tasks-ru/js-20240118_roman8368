30e6b045bbb7559201fa3eae2bf2106385f5c595
oqo0 <oq.roman@gmail.com>
2024-02-06 22:05:59 +0300

    Complete 3 task

--- 02-javascript-data-types/3-omit/index.js
+++ 02-javascript-data-types/3-omit/index.js
@@ -5,5 +5,13 @@
  * @returns {object} - returns the new object
  */
 export const omit = (obj, ...fields) => {
+    const result = {};
 
+    for (const key of Object.entries(obj)) {
+        if (!fields.includes(key[0])) {
+            result[key[0]] = key[1];
+        }
+    }
+
+    return result;
 };
